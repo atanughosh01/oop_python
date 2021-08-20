7ec556381675318f3827e52defa7688985a6a93f
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-08-20 19:02:48 +0530

    modified sample.py

--- classes_instances/sample.py
+++ classes_instances/sample.py
@@ -6,9 +6,18 @@ class Employee:
         self.pay = pay
         self.email = first + "." + last + "@company.com"
 
+    def fullname(self):
+        return (self.first + " " + self.last)
+
 
 emp_1 = Employee("Atanu", "Ghosh", 1000)
 emp_2 = Employee("Test", "User", 2000)
 
 print(emp_1.email)
 print(emp_2.email)
+
+print(emp_1.fullname())
+print(emp_2.fullname())
+
+print(Employee.fullname(emp_1))
+print(Employee.fullname(emp_2))
