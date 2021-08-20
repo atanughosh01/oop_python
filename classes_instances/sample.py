91f6069f160da26212283c7c8eac183a16512877
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-08-20 18:51:14 +0530

    modified sample.py

--- classes_instances/sample.py
+++ classes_instances/sample.py
@@ -1,8 +1,14 @@
 class Employee:
-    pass
+    
+    def __init__(self, first, last, pay) -> None:
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + "." + last + "@company.com"
 
-emp_1 = Employee()
-emp_2 = Employee()
 
-print(emp_1)
-print(emp_2)
+emp_1 = Employee("Atanu", "Ghosh", 1000)
+emp_2 = Employee("Test", "User", 2000)
+
+print(emp_1.email)
+print(emp_2.email)
