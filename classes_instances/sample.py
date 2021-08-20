cd11f5cbd3052104a1ffe75b981d3d140ff7c1dc
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-08-20 23:08:15 +0530

    modified sample.py

--- classes_instances/sample.py
+++ classes_instances/sample.py
@@ -1,11 +1,14 @@
 class Employee:
 
     raise_percent = 1.04
+    num_of_employees = 0
 
     def __init__(self, first, last, pay) -> None:
         self.first = first
         self.last = last
         self.pay = pay
+
+        Employee.num_of_employees += 1
 
     def fullname(self):
         return self.first + " " + self.last
@@ -16,6 +19,9 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_percent)
 
+
+print(Employee.num_of_employees)
+print("----------------------------")
 
 emp_1 = Employee("Atanu", "Ghosh", 1000)
 emp_2 = Employee("Test", "User", 2000)
@@ -33,3 +39,7 @@ print(emp_2.email())
 print(emp_2.pay)
 emp_2.apply_raise()
 print(emp_2.pay)
+
+print("----------------------------")
+
+print(Employee.num_of_employees)
