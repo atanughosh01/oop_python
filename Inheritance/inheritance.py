4e02b6fb545b8d1030017953a1ac95e93f762eb1
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-08-21 14:36:25 +0530

    modified inheritance.py

--- Inheritance/inheritance.py
+++ Inheritance/inheritance.py
@@ -13,20 +13,45 @@ class Employee:
         return self.first + " " + self.last
 
     def email(self):
-        return self.first + "." + self.last + "@company.com"
+        return (self.first + "." + self.last + "@company.com").lower()
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
 
 class Developer(Employee):
-    pass
+    raise_amount = 1.10
+
+    def __init__(self, first, last, pay, prog_lang) -> None:
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
 
-d1 = Developer("Atanu", "Ghosh", 1000)
-d2 = Developer("Test", "User", 2000)
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employee_list=None) -> None:
+        super().__init__(first, last, pay)
+        if employee_list is None:
+            self.employee_list = []
+        else:
+            self.employee_list = employee_list
+
+    def add_emp(self, emp):
+        if emp not in self.employee_list:
+            self.employee_list.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employee_list:
+            self.employee_list.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employee_list:
+            print("-->", emp)
+            print("-->", emp.fullname())
+
+
+d1 = Developer("Atanu", "Ghosh", 1000, "Python")
+d2 = Developer("Test", "User", 2000, "Java")
 
 print(d1.email())
-print(d2.email())
-
-print(help(Developer))
+print(d1.prog_lang)
